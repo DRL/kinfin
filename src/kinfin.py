a4204415e090506f166e62ee9952b112ffe5061c
Dominik R Laetsch <dom@dombadom.local>
2017-05-31 15:36:53 +0100

    Revert debug

--- src/kinfin.py
+++ src/kinfin.py
@@ -151,7 +151,7 @@ def statistic(count_1, count_2, test):
         mean_count_1 = mean(implicit_count_1)
         mean_count_2 = mean(implicit_count_2)
         log2_mean = log((mean(implicit_count_1)/mean(implicit_count_2)), 2)
-        print implicit_count_1, implicit_count_2, log2_mean
+        #print implicit_count_1, implicit_count_2, log2_mean
         if set(implicit_count_1) == set(implicit_count_2): # equal
             pvalue = 1.0
         elif test == "welch":
