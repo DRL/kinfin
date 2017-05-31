d8dfc230e2cd894b493585a9a7b4738569836e14
Dominik R Laetsch <dom@dombadom.local>
2017-05-31 15:34:02 +0100

    Fix statistics

--- src/kinfin.py
+++ src/kinfin.py
@@ -152,7 +152,9 @@ def statistic(count_1, count_2, test):
         mean_count_2 = mean(implicit_count_2)
         log2_mean = log((mean(implicit_count_1)/mean(implicit_count_2)), 2)
         print implicit_count_1, implicit_count_2, log2_mean
-        if test == "welch":
+        if set(implicit_count_1) == set(implicit_count_2): # equal
+            pvalue = 1.0
+        elif test == "welch":
             #try:
             pvalue = scipy.stats.ttest_ind(implicit_count_1, implicit_count_2, equal_var = False)[1] # Welch's t-test
             if pvalue != pvalue: # testing for "nan"
