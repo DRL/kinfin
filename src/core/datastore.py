84d4032c68f35a450dd9c731189e3fc888d25387
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-16 19:35:57 +0530

    feat: initialize AloCollection inside data store

---
+++ src/core/datastore.py
@@ -0,0 +1,27 @@
+from core.alo_collections import AloCollection
+from core.build import build_AloCollection, build_ALoCollection_from_json
+from core.input import InputData
+
+
+class DataFactory:
+    def __init__(self, inputData: InputData) -> None:
+        self.dirs = {}
+        self.inputData: InputData = inputData
+        if isinstance(self.inputData.config_data, str):
+            self.aloCollection: AloCollection = build_AloCollection(
+                config_f=self.inputData.config_data,
+                nodesdb_f=self.inputData.nodesdb_f,
+                tree_f=self.inputData.tree_f,
+                taxranks=self.inputData.taxranks,
+            )
+        elif self.inputData.taxon_idx_mapping_file is not None:
+            self.aloCollection: AloCollection = build_ALoCollection_from_json(
+                nodesdb_f=self.inputData.nodesdb_f,
+                tree_f=self.inputData.tree_f,
+                taxranks=self.inputData.taxranks,
+                json_list=self.inputData.config_data,
+                taxon_idx_mapping_file=self.inputData.taxon_idx_mapping_file,
+            )
+            pass
+        else:
+            raise ValueError("[ERROR] - Either provide config file or json")
