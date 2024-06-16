ebebba5aa5d16d27e5fd0f15e59e1dc7da507ad3
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-17 00:21:18 +0530

    feat: add method to setup directories to store output

--- src/core/datastore.py
+++ src/core/datastore.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+from typing import Optional
+
 from core.alo_collections import AloCollection
 from core.build import (
     build_AloCollection,
@@ -50,3 +54,50 @@ class DataFactory:
             proteinCollection=self.proteinCollection,
             infer_singletons=self.inputData.infer_singletons,
         )
+
+    def setup_dirs(self):
+        """
+        Set up output directories for storing results and attributes.
+        """
+        output_path: Optional[str] = self.inputData.output_path
+
+        if output_path:
+            if not os.path.isabs(output_path):
+                output_path = os.path.abspath(output_path)
+        else:
+            output_path = os.path.join(os.getcwd(), "kinfin_results")
+
+        self.dirs["main"] = output_path
+        print(f"[STATUS] - Output directories in \n\t{output_path}")
+        if os.path.exists(output_path):
+            print("[STATUS] - Directory exists. Deleting directory ...")
+            shutil.rmtree(output_path)
+
+        print("[STATUS] - Creating directories ...")
+        os.mkdir(output_path)
+        for attribute in self.aloCollection.attributes:
+            print(attribute)
+            attribute_path = os.path.join(output_path, attribute)
+            self.dirs[attribute] = attribute_path
+            if not os.path.exists(attribute_path):
+                print(f"\t{attribute_path}")
+                os.mkdir(attribute_path)
+
+        if self.aloCollection.tree_ete is not None:
+            tree_path = os.path.join(output_path, "tree")
+            node_chart_path = os.path.join(tree_path, "charts")
+            node_header_path = os.path.join(tree_path, "headers")
+
+            if not os.path.exists(tree_path):
+                print(f"\t{tree_path}")
+                os.mkdir(tree_path)
+                self.dirs["tree"] = tree_path
+
+                print(f"\t{node_chart_path}")
+                os.mkdir(node_chart_path)
+                self.dirs["tree_charts"] = node_chart_path
+
+                if self.inputData.plot_tree:
+                    print(f"\t{node_header_path}")
+                    os.mkdir(node_header_path)
+                    self.dirs["tree_headers"] = node_header_path
