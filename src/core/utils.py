d87a75a2e8a8ac9fd3bc3d1a958bcaffbb8011b7
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-16 19:08:17 +0530

    feat: added util function to read file line by line

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,4 +1,6 @@
+import gzip
 import os
+from typing import Any, Generator
 
 
 def check_file(filepath: str | None, install_kinfin: bool = False) -> None:
@@ -17,3 +19,25 @@ def check_file(filepath: str | None, install_kinfin: bool = False) -> None:
         if install_kinfin:
             error_msg += " Please run the install script to download kinfin."
         raise FileNotFoundError(error_msg)
+
+
+def yield_file_lines(filepath: str) -> Generator[str, Any, None]:
+    """
+    Args:
+        filepath (str): Path to the file.
+
+    Yields:
+        str: Each line from the file.
+    """
+    check_file(filepath)
+    if filepath.endswith(".gz"):
+        with gzip.open(filepath, "rb") as fh:
+            for line in fh:
+                line = line.decode("utf-8")
+                if line.startswith("nodesDB.txt"):
+                    line = "#%s" % line.split("#")[1]
+                yield line.rstrip("\n")
+    else:
+        with open(filepath) as fh:
+            for line in fh:
+                yield line.rstrip("\n")
