16697b5ddcde98a92464c307d362293cdb9a0fca
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-16 16:19:15 +0530

    feat: added constants to config.py file

---
+++ src/core/config.py
@@ -0,0 +1,17 @@
+ATTRIBUTE_RESERVED = ["IDX", "OUT", "TAXID"]
+SUPPORTED_TESTS = set(["welch", "mannwhitneyu", "ttest", "ks", "kruskal"])
+SUPPORTED_PLOT_FORMATS = set(["png", "pdf", "svg"])
+SUPPORTED_TAXRANKS = set(
+    [
+        "superkingdom",
+        "kingdom",
+        "phylum",
+        "class",
+        "order",
+        "superfamily",
+        "family",
+        "subfamily",
+        "genus",
+        "species",
+    ]
+)
