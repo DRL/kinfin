b1573df85c2635ae97241229de41e0f51bc04538
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-16 18:31:49 +0530

    feat: added method to compute proteomes grouped by levels for each attribute

--- src/core/alo_collections.py
+++ src/core/alo_collections.py
@@ -27,3 +27,30 @@ class AloCollection:
         self.level_by_attribute_by_proteome_id = level_by_attribute_by_proteome_id
         self.node_idx_by_proteome_ids = node_idx_by_proteome_ids
         self.tree_ete = tree_ete
+        self.proteome_ids_by_level_by_attribute = self.compute_proteomes_by_level_by_attribute()  # fmt: skip
+
+    def compute_proteomes_by_level_by_attribute(self) -> Dict[str, Dict[str, Set[str]]]:
+        """
+        Compute proteomes grouped by levels for each attribute.
+
+        Args:
+            attributes (List[str]): A list of strings representing attributes.
+            level_by_attribute_by_proteome_id (Dict[str, Dict[str, str]]): A dictionary where keys
+                are proteome IDs (strings), and values are dictionaries with keys representing
+                attributes (strings) and values representing levels (strings).
+
+        Returns:
+            Dict[str, Dict[str, Set[str]]]: A dictionary where keys are attributes (strings),
+                and values are dictionaries. The inner dictionaries have keys representing
+                levels (strings) and values representing sets of proteome IDs (strings).
+        """
+        proteomes_by_level_by_attribute: Dict[str, Dict[str, Set[str]]] = {
+            attribute: {} for attribute in self.attributes
+        }
+        for proteome_id in self.level_by_attribute_by_proteome_id:
+            for attribute in self.attributes:
+                level = self.level_by_attribute_by_proteome_id[proteome_id][attribute]
+                if level not in proteomes_by_level_by_attribute[attribute]:
+                    proteomes_by_level_by_attribute[attribute][level] = set()
+                proteomes_by_level_by_attribute[attribute][level].add(proteome_id)
+        return proteomes_by_level_by_attribute
