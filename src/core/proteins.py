15d3c119af6450fca55b9144b14715036f2a4f74
Rohan Barsagade <rbarsagade101@gmail.com>
2024-06-16 21:13:47 +0530

    feat: added method to add annotations to protein

--- src/core/proteins.py
+++ src/core/proteins.py
@@ -36,3 +36,34 @@ class ProteinCollection:
         self.fastas_parsed: bool = False
         self.functional_annotation_parsed: bool = False
         self.domain_desc_by_id_by_source: Dict[str, Dict[str,str]] = {}  # fmt: skip
+
+    def add_annotation_to_protein(
+        self,
+        domain_protein_id: str,
+        domain_counter_by_domain_source: Dict[str, Counter],
+        go_terms: List[str],
+    ):
+        """
+        Updates a protein object with domain counters and GO terms.
+
+        Args:
+        - domain_protein_id (str): Identifier of the protein to annotate.
+        - domain_counter_by_domain_source (Dict[str, Counter]): Domain sources mapped to counters of domains.
+        - go_terms (List[str]): Gene Ontology (GO) terms associated with the protein.
+
+        This method sets domain counters, assigns GO terms, and checks if the protein is secreted
+        based on domain information ('SignalP_EUK' source).
+
+        Note: If 'SignalP_EUK' indicates 'SignalP-noTM', sets protein.secreted = True.
+        """
+        protein: Optional[Protein] = self.proteins_by_protein_id.get(
+            domain_protein_id, None
+        )
+        if protein is not None:
+            protein.domain_counter_by_domain_source = domain_counter_by_domain_source
+            signalp_notm = protein.domain_counter_by_domain_source.get(
+                "SignalP_EUK", None
+            )
+            if signalp_notm and "SignalP-noTM" in signalp_notm:
+                protein.secreted = True
+            protein.go_terms = go_terms
