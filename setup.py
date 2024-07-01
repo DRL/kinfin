256074695a6ed976b84f3d0d84b870bc70caa9a5
DRL <dominik.laetsch@gmail.com>
2024-07-01 14:40:40 +0100

    Update setup.py
    - added `python_requires=">3.10",` since some syntax does not work on earlier versions
    - black linter likes double quotes

--- setup.py
+++ setup.py
@@ -1,37 +1,38 @@
 import pip
 from setuptools import setup, find_packages
 
-__version__ = '1.1'
+__version__ = "1.1"
 
 # Get the long description from the README file
-with open('README.md', 'r') as readme:
+with open("README.md", "r") as readme:
     long_description = readme.read()
 
 # get the dependencies and installs
-with open('requirements.txt', 'r') as requirements:
+with open("requirements.txt", "r") as requirements:
     reqs = requirements.read().splitlines()
 
 setup(
-    name='kinfin',
+    name="kinfin",
+    python_requires=">3.10",
     version=__version__,
-    description='Taxon-aware analysis of clustered protein data',
+    description="Taxon-aware analysis of clustered protein data",
     long_description=long_description,
-    url='https://github.com/DRL/kinfin',
-    download_url='https://github.com/DRL/kinfin/tarball/' + __version__,
-    license='GnuGPL3',
+    url="https://github.com/DRL/kinfin",
+    download_url="https://github.com/DRL/kinfin/tarball/" + __version__,
+    license="GnuGPL3",
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
     ],
-    keywords='Comparative genomics',
-    packages=find_packages(exclude=['docs', 'tests*']),
+    keywords="Comparative genomics",
+    packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
-    author='Dominik R Laetsch',
+    author="Dominik R Laetsch",
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "kinfin=src.kinfin:main",
-            ],
-        },
-    author_email='dominik.laetsch@gmail.com'
+        ],
+    },
+    author_email="dominik.laetsch@gmail.com",
 )
